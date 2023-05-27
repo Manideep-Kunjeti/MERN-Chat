7638f01d2edf1b7aafd25804955e302887c566fe
Manideep22 <1by20ec085@bmsit.in>
2023-05-28 00:17:17 +0530

    Added search user api and authMiddleware

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -49,6 +49,7 @@ const authUser = asyncHandler(async (req, res) => {
             _id: user._id,
             name: user.username,
             email: user.email,
+            token: generateToken(user._id),
             pic: user.pics
         })
     } else {
@@ -57,4 +58,17 @@ const authUser = asyncHandler(async (req, res) => {
     }
 })
 
-module.exports = { registerUser, authUser }
+// /api/user?search=mani
+const allUsers = asyncHandler(async (req, res) => {
+    const keyword = req.query.search ? {
+        $or: [
+            { username: { $regex: req.query.search, $options: 'i' } },
+            { email: { $regex: req.query.search, $options: 'i' } }
+        ]
+    } : {}
+
+    const users = await User.find(keyword).find({ _id: { $ne: req.user._id } })
+    res.send(users)
+})
+
+module.exports = { registerUser, authUser, allUsers }
